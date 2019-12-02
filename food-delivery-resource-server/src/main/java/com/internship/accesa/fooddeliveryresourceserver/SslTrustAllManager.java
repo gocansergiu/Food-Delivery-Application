3434669ba0899163ce1399ed7185a4fbd4481f2c
Daniel, Hodrea (Accesa IT Systems) <daniel.hodrea@accesa.eu>
2019-12-02 15:43:28 +0200

    Secured auth server and resource server demo apps with SSL. Added facebook provider as a second OAuth2 Identity for resource server.

---
+++ food-delivery-resource-server/src/main/java/com/internship/accesa/fooddeliveryresourceserver/SslTrustAllManager.java
@@ -0,0 +1,56 @@
+package com.internship.accesa.fooddeliveryresourceserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+
+@Component
+public class SslTrustAllManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(SslTrustAllManager.class);
+
+    private SSLContext sslContext;
+
+    public SslTrustAllManager(@Value("${spring.security.trust-all-certificates}") Boolean trustAll){
+        if(trustAll != null && trustAll.booleanValue()){
+            TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                        public void checkClientTrusted(
+                                java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                        public void checkServerTrusted(
+                                java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            try {
+                SSLContext sc = SSLContext.getInstance("SSL");
+                sc.init(null, trustAllCerts, new java.security.SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+                this.sslContext = sc;
+            }
+            catch (GeneralSecurityException e) {
+
+                logger.warn("[EXCEPTION] - Unable to trust all SSL certs", e);
+            }
+        }
+    }
+
+    public SSLContext getSSLContext(){
+        return this.sslContext;
+    }
+}
