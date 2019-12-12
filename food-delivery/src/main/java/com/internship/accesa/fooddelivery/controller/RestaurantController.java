8d48d7e35822c1aa770252c4bf80d5b8cfe02210
Sergiu Gocan <sergiu.gocan@accesa.eu>
2019-12-12 17:28:12 +0200

    Merge branch 'Marius' of bitbucket.org:accesa_internship/food_delivery into Sergiu
    # Conflicts:
    #	food-delivery/src/main/java/com/internship/accesa/fooddelivery/model/Restaurant.java

--- food-delivery/src/main/java/com/internship/accesa/fooddelivery/controller/RestaurantController.java
+++ food-delivery/src/main/java/com/internship/accesa/fooddelivery/controller/RestaurantController.java
@@ -3,15 +3,19 @@ package com.internship.accesa.fooddelivery.controller;
 import com.internship.accesa.fooddelivery.model.Restaurant;
 import com.internship.accesa.fooddelivery.repository.RestaurantRepository;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 public class RestaurantController {
 
     private RestaurantRepository repository;
@@ -20,19 +24,31 @@ public class RestaurantController {
         this.repository = repository;
     }
 
+    @GetMapping("/all")
+    public String showAll(Model model) {
+        model.addAttribute("restaurant", repository.findAll());
+        return "restaurants";
+    }
 
     @GetMapping("/restaurants")
+    @Secured({"ROLE_USER"})
+    public ModelAndView getAllRestaurants(){
+    List<Restaurant> arrayList = repository.findAll();
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.addObject("restaurants",arrayList);
+    return modelAndView;}
+ /*   @GetMapping("/restaurants")
     @Secured({"ROLE_USER"})
     public List<Restaurant> getAllRestaurants(){
         //DUMMY get all restaurants controller
        // return Arrays.asList("One restaurant", "Another restaurant");
   //      ModelAndView modelAndView = new ModelAndView();
-        
+
      return repository.findAll();
 
 
     }
-
+*/
     @GetMapping("/restaurants/find/{decisionId}")
     public Optional<Restaurant> findone(@PathVariable @NotNull @DecimalMin("0") Long decisionId ){
             return repository.findById(decisionId);
