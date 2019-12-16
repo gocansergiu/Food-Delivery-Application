da2f63826f6ceaf9256b60bf778b43ac3f745ef5
Marius Butaciu <marius.butaciu@accesa.eu>
2019-12-16 14:02:44 +0200

    Update database. Made relationship between Dishes and Restaurants. Also updated Models for each of them

--- food-delivery/src/main/java/com/internship/accesa/fooddelivery/model/Restaurant.java
+++ food-delivery/src/main/java/com/internship/accesa/fooddelivery/model/Restaurant.java
@@ -14,4 +14,42 @@ public class Restaurant {
     @Column(name = "restaurant_name")
     private String restaurantName;
 
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "photo_link")
+    private String photoLink;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhotoLink() {
+        return photoLink;
+    }
+
+    public void setPhotoLink(String photoLink) {
+        this.photoLink = photoLink;
+    }
 }
